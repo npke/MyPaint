4260077010a2a855217853ad7bf1e8d362b2b41d
Ke Nguyen Phu <kenguyen276@gmail.com>
2016-06-14 22:59:59 +0700

    Add ShapeManager class to manage object

---
+++ MyPaint/ToggleButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+namespace MyPaint
+{
+    class ToggleButtonManager
+    {
+        private static List<ToggleButton> toggleBtnList = new List<ToggleButton>();
+
+        public static void AddToggleButton(ToggleButton toggleButton)
+        {
+            toggleBtnList.Add(toggleButton);
+        }
+
+        public static void CheckButton(ToggleButton checkedBtn)
+        {
+            foreach (ToggleButton btn in toggleBtnList)
+            {
+                if (btn != checkedBtn)
+                    btn.IsChecked = false;
+            }
+        }
+    }
+}
